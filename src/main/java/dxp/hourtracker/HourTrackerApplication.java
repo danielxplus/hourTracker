46e3fc7998fa2ccd128ceaa3b36dd96dd117b496
Daniel <danielav@edu.hac.ac.il>
2025-12-16 23:20:33 +0200

    Initial commit: backend + frontend

---
+++ src/main/java/dxp/hourtracker/HourTrackerApplication.java
@@ -0,0 +1,13 @@
+package dxp.hourtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourTrackerApplication.class, args);
+	}
+
+}
